1b1eb97a5b63568a0fdc10ec99ea5ad8da5e34d9
Jon Jackson <jonjacksonvibes@gmail.com>
2024-06-04 17:02:24 -0500

    crop add modal added

--- client/src/pages/Crops/Crops.tsx
+++ client/src/pages/Crops/Crops.tsx
@@ -1,4 +1,16 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Plus } from "lucide-react";
 
 export const Crops = () => {
@@ -7,7 +19,31 @@ export const Crops = () => {
       <Card className="flex flex-col items-center rounded-md mx-3 w-1/4">
         <CardHeader className="text-md font-semibold flex flex-row justify-between py-2 text-neutral-50 w-full bg-neutral-600 rounded-t">
           <span className="mr-3">Crop Library</span>
-          <Plus />
+          <Dialog>
+            <DialogTrigger asChild>
+              <Plus />
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+              <DialogHeader>
+                <DialogTitle>Add crop</DialogTitle>
+              </DialogHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="name" className="text-right">
+                    Crop Name
+                  </Label>
+                  <Input
+                    id="name"
+                    value="Pedro Duarte"
+                    className="col-span-3"
+                  />
+                </div>
+              </div>
+              <DialogFooter>
+                <Button type="submit">Add crop!</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
         </CardHeader>
         <CardContent></CardContent>
       </Card>
@@ -21,6 +57,7 @@ export const Crops = () => {
       <Card className="flex flex-col items-center rounded-md mx-3 w-1/4">
         <CardHeader className="text-md font-semibold flex flex-row justify-between py-2 text-neutral-50 w-full bg-neutral-600 rounded-t">
           <span className="mr-3">Crop Info</span>
+
           <Plus />
         </CardHeader>
         <CardContent></CardContent>
